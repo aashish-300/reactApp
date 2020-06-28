d131ed32cdf5b9f1add389fcf25d7b52dc05f140
aashish-300 <59108668+aashish-300@users.noreply.github.com>
2020-06-28 12:52:22 +0545

    This is the first modification in github file

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { AppRegistry, Text, View, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import AsyncStorage from '@react-native-community/async-storage'
 
+//This is just for try 
+//This is my first time with git and github
 
 import RootStackScreen from './components/RootStackScreen'
 import { AuthContext } from './components/Context'
